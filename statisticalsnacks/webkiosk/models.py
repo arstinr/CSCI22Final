9d728c7764e5a975bac03c6df4c0abd9bf37913c
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-04 23:04:47 +0800

    Created Customer Class (migrated)

--- statisticalsnacks/webkiosk/models.py
+++ statisticalsnacks/webkiosk/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'''CUSTOMER #{self.id}
+NAME: {self.firstname}
+ADDRESS: {self.address}
+CITY: {self.city}'''
