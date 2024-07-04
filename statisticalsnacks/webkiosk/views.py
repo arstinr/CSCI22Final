61e8807ecc435d02b7353c4de824b89d63e8cc5a
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-04 22:52:38 +0800

    Created Base Template

--- statisticalsnacks/webkiosk/views.py
+++ statisticalsnacks/webkiosk/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<p>Welcome to Statistical Snacks webkiosk!</p>')
+    return render(request, "webkiosk/base_template.html")
