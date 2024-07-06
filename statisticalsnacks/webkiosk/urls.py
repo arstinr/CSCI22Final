81fc93d835a5469a386a95c4a3f1c89e86b6fdae
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-06 23:29:13 +0800

    Customer list context

--- statisticalsnacks/webkiosk/urls.py
+++ statisticalsnacks/webkiosk/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('customers/', views.listcustomers, name='customer-list')
 ]
--- statisticalsnacks/webkiosk/views.py
+++ statisticalsnacks/webkiosk/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Customer, Address
 
 # Create your views here.
 
 def index(request):
     return render(request, "webkiosk/base_template.html")
+
+def listcustomers(request):
+    customerlist = Customer.objects.all()
+    context = {'customerlist': customerlist}
+    return render(request, "webkiosk/customer_list.html", context)
