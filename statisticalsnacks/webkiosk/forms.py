952aad79af006b5c6145681c23b81e0057401d18
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-06 23:40:46 +0800

    addcustomer view

--- statisticalsnacks/webkiosk/forms.py
+++ statisticalsnacks/webkiosk/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import Customer
 
-class CustomerForm(ModelForm):
+class AddCustomerForm(ModelForm):
     class Meta:
         model = Customer
         fields = ['firstname', 'lastname']
--- statisticalsnacks/webkiosk/views.py
+++ statisticalsnacks/webkiosk/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Customer, Address
+from .forms import AddCustomerForm
 
 # Create your views here.
 
@@ -11,3 +12,14 @@ def listcustomers(request):
     customerlist = Customer.objects.all()
     context = {'customerlist': customerlist}
     return render(request, "webkiosk/customer_list.html", context)
+
+def addcustomer(request):
+    if request.method == 'GET':
+        acf = AddCustomerForm()
+    elif request.method == 'POST':
+        acf = AddCustomerForm(request.POST)
+        if acf.is_valid():
+            acf.save()
+    
+    context = {'form': acf}
+    return render(request, 'webkiosk/customer_form.html', context)
