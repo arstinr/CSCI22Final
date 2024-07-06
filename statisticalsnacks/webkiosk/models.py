56bed54c5aafabe62455df3528595d2edd8cd2a7
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-06 23:32:08 +0800

    Customer model property edit

--- statisticalsnacks/webkiosk/models.py
+++ statisticalsnacks/webkiosk/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Customer(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
-    date_joined = models.DateField(auto_now_add=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'''CUSTOMER #{self.id}
