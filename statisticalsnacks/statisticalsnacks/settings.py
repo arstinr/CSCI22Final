939650351c01bdb97be2eff1d57becb804b14343
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-07 23:04:45 +0800

    Add localhost to ALLOWED_HOSTS

--- statisticalsnacks/statisticalsnacks/settings.py
+++ statisticalsnacks/statisticalsnacks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0^^e(cn_xummkzx!-zjj-na*pni$gm@q!qw5bd5fz6wdjewol7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
