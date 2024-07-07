4d6b4b9a3e1d7cdbabefb43d9c42ee0d4656bb70
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-08 01:00:06 +0800

    Made Migrations and minor edits

--- statisticalsnacks/webkiosk/models.py
+++ statisticalsnacks/webkiosk/models.py
@@ -22,7 +22,7 @@ class Address(models.Model):
 class Food(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    price = models.DecimalField(max_length=8)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
 
     def __str__(self):
         return f'''{self.name}: {self.description}
@@ -42,7 +42,7 @@ class Order(models.Model):
         return f'''Ordered: {self.orderdatetime}
 Payment Method: {self.paymentmode}'''
 class OrderItem(models.Model):
-    quantity = models.IntegerField(max_length=3)
+    quantity = models.IntegerField()
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
 
