d85944a5a199d9305f4eb2bf5ee5cf9675096252
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-08 00:04:18 +0800

    Import AddAddressForm to views.py

--- statisticalsnacks/webkiosk/views.py
+++ statisticalsnacks/webkiosk/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import Customer, Address
-from .forms import AddCustomerForm
+from .forms import AddCustomerForm, AddAddressForm
 
 # Create your views here.
 
